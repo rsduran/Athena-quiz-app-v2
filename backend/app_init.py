907d1d47b77f55594f12d0f7573f4c96d5d881d1
Reineir Duran <reineirduran@Amihans-MacBook-Air.local>
2024-10-13 22:42:48 +0800

    Updated files

--- backend/app_init.py
+++ backend/app_init.py
@@ -61,7 +61,7 @@ print(f"[DEBUG] Database URI: postgresql://{DB_USER}:{'*' * len(DB_PASS)}@{DB_HO
 db.init_app(app)
 
 # Configure CORS
-CORS(app, resources={r"/api/*": {"origins": app.config['FRONTEND_URL'], "supports_credentials": True}})
+CORS(app, resources={r"/api/*": {"origins": "http://k8s-threetie-mainlb-7703746d77-255087660.ap-southeast-2.elb.amazonaws.com", "supports_credentials": True}})
 print("[DEBUG] CORS configured")
 
 # Session configuration
